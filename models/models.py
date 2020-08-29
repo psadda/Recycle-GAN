8a2750f44d51e518453eb235e9850d2a30e07ec9
Praneeth Sadda <psadda@gmail.com>
2020-08-29 17:55:09 -0400

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,7 +1,6 @@
 
 def create_model(opt):
     model = None
-    print(opt.model)
     if opt.model == 'cycle_gan':
         assert(opt.dataset_mode == 'unaligned')
         from .cycle_gan_model import CycleGANModel
@@ -10,12 +9,7 @@ def create_model(opt):
         assert(opt.dataset_mode == 'unaligned_triplet')
         from .recycle_gan_model import RecycleGANModel
         model = RecycleGANModel()
-    elif opt.model == 'reCycle_gan':
-        assert(opt.dataset_mode == 'unaligned_triplet')
-        from .reCycle_gan_model import ReCycleGANModel
-        model = ReCycleGANModel()
     else:
         raise ValueError("Model [%s] not recognized." % opt.model)
     model.initialize(opt)
-    print("model [%s] was created" % (model.name()))
     return model
